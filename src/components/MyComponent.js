2aa78c5f4e44c8a2912a24047ce76490d8ca5dbf
duyanhDev <loveeasy20@gmail.com>
2024-05-13 21:52:05 +0700

    check 3

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,23 +1,9 @@
 import React from "react";
-
+import Userinfo from "./Userinfor";
 class MyComponent extends React.Component {
-  state = {
-    name: "Đặng Trịnh Duy Anh",
-    address: "Bình Dương",
-    age: 26,
-  };
   // JSX
   render() {
-    const { name, address, age } = this.state;
-    return (
-      <>
-        <p>Anh yêu đôn nhiều lắm chuppa chup ....</p>
-        {Math.random()}
-        <p>{name}</p>
-        <p>{address}</p>
-        <p>{age}</p>
-      </>
-    );
+    return <Userinfo></Userinfo>;
   }
 }
 
